7d43a569d58763d1fefa28b2434e559b0d81f15e
Jan Dvořák <mordae@anilinux.org>
2016-01-14 09:56:43 +0100

    Use unix-socket instead of unstable-lib
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "misc1"))
-(define build-deps '("racket-doc" "unstable-doc"))
+(define deps '("base" "scribble-lib" "misc1" "unix-socket"))
+(define build-deps '("racket-doc" "unstable-doc" "unix-socket"))
 
 ; vim:set ts=2 sw=2 et:
