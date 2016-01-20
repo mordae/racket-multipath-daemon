4d8a2644d2641e9d263e83caef28b3bf6af63b88
Jan Dvořák <mordae@anilinux.org>
2016-01-20 17:56:12 +0100

    info.rkt: s/unix-socket/unix-socket-{lib,doc}/
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "misc1" "unix-socket"))
-(define build-deps '("racket-doc" "unstable-doc" "unix-socket"))
+(define deps '("base" "scribble-lib" "misc1" "unix-socket-lib"))
+(define build-deps '("racket-doc" "unstable-doc" "unix-socket-doc"))
 
 ; vim:set ts=2 sw=2 et:
